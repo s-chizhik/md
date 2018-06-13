516ecf146701491dd54c5ac4a79f3175940956d2
Sergey Chizhik <s.chizhik@dinarys.com>
2018-06-13 16:47:29 +0300

    template + lang

--- lang/en.py
+++ lang/en.py
@@ -7,10 +7,19 @@ texts = {
     'head.lang': name,
     'head.title': 'Mission Day Dnipro',
     'head.author': 'Dnipro',
+
+    'countdown.to': 'To',
+    'countdown.mission_day': 'Mission Day',
+    'countdown.left': 'left',
+
+    'countdown.days': 'Days',
+    'countdown.hours': 'Hours',
+    'countdown.minutes': 'Minutes',
+    'countdown.seconds': 'Seconds',
+
     'body.template': """
         Agents, we want to discover Dnipro on 11th of August, 2018.
         In collaboration with the City of Dnipro we will provide you high quality missions to explore the city
         on foot together. Sign up to receive more information soon.
     """,
 }
-
--- lang/ua.py
+++ lang/ua.py
@@ -7,6 +7,16 @@ texts = {
     'head.lang': name,
     'head.title': 'Mission Day Дніпро',
     'head.author': 'Дніпро',
+
+    'countdown.to': 'До',
+    'countdown.mission_day': 'Mission Day',
+    'countdown.left': 'лишилось',
+
+    'countdown.days': 'Днів',
+    'countdown.hours': 'Годин',
+    'countdown.minutes': 'Хвилин',
+    'countdown.seconds': 'Секунд',
+
     'body.template': """
         Агенти, ми хочемо відкрити Дніпро 11 серпня 2018 року.
          У співпраці з м. Дніпро ми забезпечимо вам високоякісні місії для вивчення міста
