2cb6b30c98937b00c1ccf30e57dc5667c451660d
Marianna <marianna@loopme.com>
2018-08-10 14:19:56 +0300

    added scrollspy

--- assets/js/missionday.js
+++ assets/js/missionday.js
@@ -52,3 +52,26 @@ function getTimeRemaining(endtime) {
   })
 
   document.getElementById(document.documentElement.lang).classList.add("active");
+
+  (function() {
+    'use strict';
+  
+    var section = document.querySelectorAll("section");
+    var sections = {};
+    var i = 0;
+  
+    Array.prototype.forEach.call(section, function(e) {
+      sections[e.id] = e.offsetTop;
+    });
+  
+    window.onscroll = function() {
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  
+      for (i in sections) {
+        if (sections[i] <= scrollPosition) {
+          document.querySelector('.navigation .active').classList.remove('active');
+          document.querySelector('a[href="#' + i + '"]').classList.add('active');
+        }
+      }
+    };
+  })();
